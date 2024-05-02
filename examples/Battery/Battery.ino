82b9d223573bd7380ba3c3c91c71c8297bff1d12
Sebastian Romero <s.romero@arduino.cc>
2024-05-02 14:03:15 +0200

    Use new battery API in example

--- examples/Battery/Battery.ino
+++ examples/Battery/Battery.ino
@@ -12,11 +12,11 @@
   - PowerManagement library (installable from the Arduino Library Manager)
 
   Usage:
-  1. Install the required library:
-     - Open the Arduino IDE.
-     - Go to "Sketch" -> "Include Library" -> "Manage Libraries..."
-     - Search for "Arduino_PowerManagement" and install it.
 
+  1. Connect the Battery:
+     - Please note that batteries without NTC thermistors will 
+       not provide temperature information.
+  
   2. Upload the Sketch:
      - Open the provided sketch in the Arduino IDE.
      - Select your board type and port from the "Tools" menu.
@@ -28,39 +28,85 @@
      - You will see the sketch continuously printing battery information.
 */
 
-#include "PowerManagement.h"
+#include "Arduino_PowerManagement.h"
 
-PowerManagement manager;
 Battery battery; 
+Charger charger;
 
 void setup() {
    Serial.begin(115200);
    while (!Serial);
+   delay(1000); // Delay to give time to load the Serial Monitor
 
-   // Battery capacity in mAh. Change this value to match your battery's capacity.
-   auto batteryCapacity = 200;
 
-   manager = PowerManagement();
-   manager.begin();
-   
-   battery = Battery(batteryCapacity);
-   bool batteryInitialized = battery.begin();
+   // Charger charger = Charger();
+   // charger.begin();
+   // charger.setEndOfChargeCurrent(5); // Set the end of charge current in mA
+   Serial.println("Charger initialized.");
+
+   BatteryCharacteristics characteristics = BatteryCharacteristics();
+   characteristics.capacity = 200; // Battery capacity in mAh. Change this value to match your battery's capacity.
+   characteristics.endOfChargeCurrent = 5; // End of charge current in mA
+
+   battery = Battery(characteristics);
+   bool batteryInitialized = battery.begin(true);
    
    if (!batteryInitialized) {
       Serial.println("Battery initialization failed.");
+      Serial.println("Please make sure the battery is connected and try again.");
       while (true);
    }
 
+   battery.resetMaximumMinimumVoltage();
+}
+
+void printTimeToEmpty(){
+   auto timeToEmptySeconds = battery.timeToEmpty();
+   if(timeToEmptySeconds == -1){
+      Serial.println("* Time to empty: N/A");
+      return;
+   }
+   auto timeToEmptyHours = timeToEmptySeconds / 3600;
+   timeToEmptySeconds = timeToEmptySeconds % 3600;
+   auto timeToEmptyMinutes = timeToEmptySeconds / 60;
+   timeToEmptySeconds = timeToEmptySeconds % 60;
+   Serial.println("* Time to empty: " + String(timeToEmptyHours) + "h " + String(timeToEmptyMinutes) + "m " + String(timeToEmptySeconds) + "s");
+}
+
+void printTimeToFull(){
+   auto timeToFullSeconds = battery.timeToFull();
+   if(timeToFullSeconds == -1){
+      Serial.println("* Time to full: N/A");
+      return;
+   }
+   auto timeToFullHours = timeToFullSeconds / 3600;
+   timeToFullSeconds = timeToFullSeconds % 3600;
+   auto timeToFullMinutes = timeToFullSeconds / 60;
+   timeToFullSeconds = timeToFullSeconds % 60;
+   Serial.println("* Time to full: " + String(timeToFullHours) + "h " + String(timeToFullMinutes) + "m " + String(timeToFullSeconds) + "s");
 }
 
 void loop() {
-   Serial.println("* Battery is connected: " + ( battery.isConnected() ? String("Yes") : String("No")));
-   Serial.println("* Voltage: " + String(battery.voltage()) + "V");
-   // Print current with a precision of 4 decimal places
-   Serial.println("* Current: " + String(battery.current(), 4) + "A");
-   Serial.println("* Percentage: " + String(battery.percentage()) + "%");
-   Serial.println("* Remaining Capacity: " + String(battery.remainingCapacity()) + "mAh");
-   Serial.println("* Temperature: " + String(battery.temperature()) + "°C");
-   Serial.println();
-   delay(1000);
+   bool batteryConnected = battery.isConnected();
+   Serial.println("* Battery is connected: " + ( batteryConnected ? String("Yes") : String("No")));
+   
+   if(batteryConnected){
+      Serial.println("* Battery is empty: " + ( battery.isEmpty() ? String("Yes") : String("No")));
+      Serial.println("* Is fully charged: " + ( battery.isFullyCharged() ? String("Yes") : String("No")));
+      Serial.println("* Voltage: " + String(battery.voltage()) + " V");
+      Serial.println("* Average Voltage: " + String(battery.averageVoltage()) + " V");
+      Serial.println("* Minimum Voltage: " + String(battery.minimumVoltage()) + " V");
+      Serial.println("* Maximum Voltage: " + String(battery.maximumVoltage()) + " V");
+      Serial.println("* Current: " + String(battery.current()) + " mA");
+      Serial.println("* Average Current: " + String(battery.averageCurrent()) + " mA");
+      Serial.println("* Percentage: " + String(battery.percentage()) + "%");
+      Serial.println("* Remaining Capacity: " + String(battery.remainingCapacity()) + " mAh");
+      Serial.println("* Full Capacity: " + String(battery.fullCapacity()) + " mAh");
+      Serial.println("* Internal Temperature: " + String(battery.internalTemperature()) + "°C");
+      Serial.println("* Average internal Temperature: " + String(battery.averageInternalTemperature()) + "°C");
+      printTimeToEmpty();
+      printTimeToFull();
+      Serial.println();
+   }
+   delay(2000);
 }
