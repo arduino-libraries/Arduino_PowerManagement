68623a087beb5b3fd569da69d2815049e23fd855
Sebastian Romero <s.romero@arduino.cc>
2024-05-09 00:50:17 +0200

    Revamp standby example

--- examples/DeepSleep_WakeFromRTC_H7/DeepSleep_WakeFromRTC_H7.ino
+++ examples/DeepSleep_WakeFromRTC_H7/DeepSleep_WakeFromRTC_H7.ino
@@ -1,20 +1,39 @@
-#include "Arduino.h"
 #include "Arduino_PowerManagement.h"
 
 Board board; 
 
-
 void setup() {
     board = Board();
-    if(!board.begin()){
-        Serial.println("Board initialization failed.");
-        while (true);
-    }
-    board.enableWakeupFromRTC();
-    board.sleepFor(0, 0, 1);
 
-    pinMode(LEDB, OUTPUT);
+    // When uploading this sketch to the M4 core, it just goes to standby mode.
+    #if defined(ARDUINO_GENERIC_STM32H747_M4)
+      board.standByUntilWakeupEvent();
+      return;
+    #endif
+
+    // Turn off the built-in LED
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    
+    // Turn on the blue LED to show that the board is still awake
+    pinMode(LEDB, OUTPUT);    
     digitalWrite(LEDB, LOW);
+
+    if(!board.begin()){
+        // If the board fails to initialize, it will blink the red LED
+        pinMode(LEDR, OUTPUT);
+        while (true){
+            digitalWrite(LEDR, LOW);
+            delay(1000);
+            digitalWrite(LEDR, HIGH);
+            delay(1000);
+        }
+    }
+
+
+    board.enableWakeupFromRTC(0, 0, 10); // Sleep for 10 seconds
+
+    // The LED should go off when the board goes to sleep
     board.setAllPeripheralsPower(false);
     board.standByUntilWakeupEvent();
 }
