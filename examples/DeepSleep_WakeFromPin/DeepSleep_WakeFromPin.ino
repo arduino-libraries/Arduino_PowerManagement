7a61e9fec521bdadaa603f1bcf63941524cf4c72
Sebastian Romero <s.romero@arduino.cc>
2024-05-08 17:14:14 +0200

    Refactor example

--- examples/DeepSleep_WakeFromPin/DeepSleep_WakeFromPin.ino
+++ examples/DeepSleep_WakeFromPin/DeepSleep_WakeFromPin.ino
@@ -4,50 +4,52 @@
 
 
 #define WAKE_PIN A3
-#define SLEEP_PIN 0
+#define GOTO_SLEEP_PIN 0
 
-volatile bool sleepFlag = false;
+volatile bool shouldGoToSleep = false;
 
-PowerManagement manager;
 Board board; 
 Charger charger;
 
 void setup() {
+    board = Board();
+    charger = Charger();
+    board.begin();
+    charger.begin();
 
-  
-    
-    manager = PowerManagement();
-    manager.begin();
-    board = manager.getBoard();
-    charger = manager.getCharger();
-
-    attachInterrupt(digitalPinToInterrupt(SLEEP_PIN), goToSleep, RISING);
+    // Allows to use a button to put the device into sleep mode
+    attachInterrupt(digitalPinToInterrupt(GOTO_SLEEP_PIN), goToSleep, RISING);
 
     #if defined(ARDUINO_PORTENTA_C33)
-    board.enableWakeupFromPin(WAKE_PIN, RISING);
+    // On Portenta C33, you can specify which pin to use to wake up the device from sleep mode
+    // Please read the documentation to understand which pins can be used to wake up the device.
+    board.enableWakeupFromPin(WAKE_UP_PIN, RISING);
     #elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_PORTENTA_H7_M4) || defined(ARDUINO_NICLA_VISION)
+    // On Portenta only pin GPIO0 can be used to wake up the device from sleep mode
     board.enableWakeupFromPin();
     #endif
 
-    
+    // TODO why is this needed? Why true?
     board.setAllPeripheralsPower(true);
-    charger.disable();
+    // TODO why is this needed?
+    charger.setEnabled(false);
 
     pinMode(LEDB, OUTPUT);
 }
 
 void goToSleep(){
-  sleepFlag = true;
+  shouldGoToSleep = true;
 }
 
 void loop() {
-    if(sleepFlag){
-        sleepFlag = false;
+    if(shouldGoToSleep){
+        shouldGoToSleep = false;
 
+        // TODO why is this here?
         //board.setAllPeripheralsPower(false);
         board.standByUntilWakeupEvent();
     } else {
-
+        // Show that the board is awake by blinking the LED
         digitalWrite(LEDB, HIGH);
         delay(1000);
         digitalWrite(LEDB, LOW);
