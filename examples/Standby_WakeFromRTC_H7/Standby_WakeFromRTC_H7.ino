5eb4791296f49589e043c8952eff6abcf6fa7340
Sebastian Romero <s.romero@arduino.cc>
2024-05-10 09:52:39 +0200

    Add documentation to low power sketch

--- examples/Standby_WakeFromRTC_H7/Standby_WakeFromRTC_H7.ino
+++ examples/Standby_WakeFromRTC_H7/Standby_WakeFromRTC_H7.ino
@@ -1,3 +1,37 @@
+/*
+  Charger Demo
+
+  This sketch demonstrates how to use the PowerManagement library enable low power modes on the Arduino Portenta H7.
+  * In the setup() function, it enters standby mode and waits for a wakeup event from the RTC.
+  * The loop() functionit is not used in this sketch.
+  
+  IMPORTANT: Please note that this sketch has to be uploaded to the M4 core too in order to achieve the lowest power consumption.
+
+  Requirements:
+  - Arduino Arduino Portenta H7
+  - Arduino IDE
+  - PowerManagement library (installable from the Arduino Library Manager)
+
+  Usage:
+
+  1. Connect a battery to the board.    
+  
+  2. Upload the Sketch to the M4 core:
+    - Open the provided sketch in the Arduino IDE.
+    - Select your board type and port from the "Tools" menu.
+    - Select the M4 core from the "Tools" menu.
+    - Click the "Upload" button to upload the sketch to your board.
+
+  3. Upload the Sketch to the M7 core:
+    - Select the M7 core from the "Tools" menu.
+    - Click the "Upload" button to upload the sketch to your board.
+
+  4. Observer LED behavior:
+    - The blue LED will turn on when the board is awake.
+    - The blue LED will turn off when the board goes to sleep.
+    - The red LED will blink if the board fails to initialize.
+*/
+
 #include "Arduino_PowerManagement.h"
 
 Board board; 
