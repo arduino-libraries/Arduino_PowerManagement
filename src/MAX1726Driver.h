a61a6e5ccf163e146601590d99b5783b0cafc85a
Sebastian Romero <s.romero@arduino.cc>
2024-06-04 10:46:19 +0200

    Extract hibernate features into function

--- src/MAX1726Driver.h
+++ src/MAX1726Driver.h
@@ -26,6 +26,15 @@ MAX1726Driver::MAX1726Driver(TwoWire *wire, uint8_t i2cAddress = DEFAULT_I2C_ADD
 
 MAX1726Driver::~MAX1726Driver(){}
 
+void MAX1726Driver::setHibernateModeEnabled(bool enabled){
+    if(enabled){
+        // Enters hibernate mode somewhere between 2.812s and 5.625s if the threshold conditions are met
+        replaceRegisterBit(this->wire, i2cAddress, HIB_CFG_REG, 1, EN_HIBERNATION_BIT); // Enter Hibernate Mode
+    } else {
+        replaceRegisterBit(this->wire, i2cAddress, HIB_CFG_REG, 0, EN_HIBERNATION_BIT); // Exit Hibernate Mode
+    }
+}
+
 bool MAX1726Driver::setOperationMode(FuelGaugeOperationMode mode) {
     if(mode == FuelGaugeOperationMode::active){
         // See section "Soft-Wakeup" in user manual https://www.analog.com/media/en/technical-documentation/user-guides/max1726x-modelgauge-m5-ez-user-guide.pdf
@@ -33,15 +42,11 @@ bool MAX1726Driver::setOperationMode(FuelGaugeOperationMode mode) {
         writeRegister16Bits(this->wire, i2cAddress, SOFT_WAKEUP_REG, 0x90); // Wakes up the fuel gauge from hibernate mode to reduce the response time of the IC to configuration changes  
         writeRegister16Bits(this->wire, i2cAddress, SOFT_WAKEUP_REG, 0x0);  // This command must be manually cleared (0x0000) afterward to keep proper fuel gauge timing
     } else if(mode == FuelGaugeOperationMode::hibernate){
-        // Enters hibernate mode somewhere between 2.812s and 5.625s if the threshold conditions are met
-        replaceRegisterBit(this->wire, I2C_ADDRESS, HIB_CFG_REG, 1, EN_HIBERNATION_BIT); // Enter Hibernate Mode
-    } else if(mode == FuelGaugeOperationMode::shutdown){
-        // Enter active mode
-        // TODO use function instead        
-        replaceRegisterBit(this->wire, I2C_ADDRESS, HIB_CFG_REG, 0, EN_HIBERNATION_BIT); // Exit Hibernate Mode
-        // TODO check if we can enter shutdown quicker by setting CTR manually in ShdnTimer
-        // The default shutdown timeout is 45s
-        replaceRegisterBit(this->wire, I2C_ADDRESS, CONFIG_REG, 1, SHDN_BIT); // Command shutdown mode
+        this->setHibernateModeEnabled(true);
+    } else if(mode == FuelGaugeOperationMode::shutdown){        
+        this->setHibernateModeEnabled(false); // Enter active mode
+        // The default (minimum) shutdown timeout is 45s
+        replaceRegisterBit(this->wire, i2cAddress, CONFIG_REG, 1, SHDN_BIT); // Command shutdown mode
     }
     
     return false;
