71326dfbe6e21b9cdcaf4861cd821a2b9def55b5
Sebastian Romero <s.romero@arduino.cc>
2024-06-04 10:45:34 +0200

    Make I2C address configurable

--- src/MAX1726Driver.h
+++ src/MAX1726Driver.h
@@ -1,27 +1,27 @@
 #include "WireUtils.h"
 #include "BatteryConstants.h"
 
-enum class FuelGaugeOperationMode
-{
+enum class FuelGaugeOperationMode {
     hibernate,
     shutdown,
     active
 };
 
-// TODO Extract into a separate header file or use this file in Battery.h
-constexpr uint8_t I2C_ADDRESS = 0x36; // I2C address of the fuel gauge
+constexpr uint8_t DEFAULT_I2C_ADDRESS = 0x36;
 
 class MAX1726Driver {
 private:
     TwoWire *wire;
+    uint8_t i2cAddress;
 public:
     bool setOperationMode(FuelGaugeOperationMode mode);
-    MAX1726Driver(TwoWire *wire);
+    MAX1726Driver(TwoWire *wire, uint8_t i2cAddress);
     ~MAX1726Driver();
 };
 
-MAX1726Driver::MAX1726Driver(TwoWire *wire) {
+MAX1726Driver::MAX1726Driver(TwoWire *wire, uint8_t i2cAddress = DEFAULT_I2C_ADDRESS) {
     this->wire = wire;
+    this->i2cAddress = i2cAddress;
 }
 
 MAX1726Driver::~MAX1726Driver(){}
@@ -29,9 +29,9 @@ MAX1726Driver::~MAX1726Driver(){}
 bool MAX1726Driver::setOperationMode(FuelGaugeOperationMode mode) {
     if(mode == FuelGaugeOperationMode::active){
         // See section "Soft-Wakeup" in user manual https://www.analog.com/media/en/technical-documentation/user-guides/max1726x-modelgauge-m5-ez-user-guide.pdf
-        writeRegister16Bits(this->wire, I2C_ADDRESS, HIB_CFG_REG, 0x0);      // Exit Hibernate Mode
-        writeRegister16Bits(this->wire, I2C_ADDRESS, SOFT_WAKEUP_REG, 0x90); // Wakes up the fuel gauge from hibernate mode to reduce the response time of the IC to configuration changes  
-        writeRegister16Bits(this->wire, I2C_ADDRESS, SOFT_WAKEUP_REG, 0x0);  // This command must be manually cleared (0x0000) afterward to keep proper fuel gauge timing
+        writeRegister16Bits(this->wire, i2cAddress, HIB_CFG_REG, 0x0);      // Exit Hibernate Mode
+        writeRegister16Bits(this->wire, i2cAddress, SOFT_WAKEUP_REG, 0x90); // Wakes up the fuel gauge from hibernate mode to reduce the response time of the IC to configuration changes  
+        writeRegister16Bits(this->wire, i2cAddress, SOFT_WAKEUP_REG, 0x0);  // This command must be manually cleared (0x0000) afterward to keep proper fuel gauge timing
     } else if(mode == FuelGaugeOperationMode::hibernate){
         // Enters hibernate mode somewhere between 2.812s and 5.625s if the threshold conditions are met
         replaceRegisterBit(this->wire, I2C_ADDRESS, HIB_CFG_REG, 1, EN_HIBERNATION_BIT); // Enter Hibernate Mode
