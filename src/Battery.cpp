2747199254418e80dcecb2474e7d3f31fd64e61c
Sebastian Romero <s.romero@arduino.cc>
2024-06-04 12:04:46 +0200

    Adjust comments

--- src/Battery.cpp
+++ src/Battery.cpp
@@ -71,10 +71,9 @@ void Battery::configureBatteryCharacteristics(){
 
 void Battery::releaseFromHibernation(){
   // See section "Soft-Wakeup" in user manual https://www.analog.com/media/en/technical-documentation/user-guides/max1726x-modelgauge-m5-ez-user-guide.pdf
-  // TODO to preserve the hiberation config, probably better to only set bit 15 to 0
-  writeRegister16Bits(this->wire, FUEL_GAUGE_ADDRESS, HIB_CFG_REG, 0x0);      // Exit Hibernate Mode
+  replaceRegisterBit(this->wire, FUEL_GAUGE_ADDRESS, HIB_CFG_REG, EN_HIBERNATION_BIT, 0); // Exit Hibernate Mode
   writeRegister16Bits(this->wire, FUEL_GAUGE_ADDRESS, SOFT_WAKEUP_REG, 0x90); // Wakes up the fuel gauge from hibernate mode to reduce the response time of the IC to configuration changes  
-  writeRegister16Bits(this->wire, FUEL_GAUGE_ADDRESS, SOFT_WAKEUP_REG, 0x0);  // This command must be manually cleared (0x0000) afterward to keep proper fuel gauge timing
+  writeRegister16Bits(this->wire, FUEL_GAUGE_ADDRESS, SOFT_WAKEUP_REG, 0x0);  // Soft wake-up must be manually cleared (0x0000) afterward to keep proper fuel gauge timing
 }
 
 bool Battery::refreshBatteryGaugeModel(){
@@ -207,7 +206,8 @@ uint8_t Battery::internalTemperature(){
   }
 
   setTemperatureMeasurementMode(false);  
-  // TODO also see DieTemp Register (034h) for the internal die temperature p. 24 in DS  
+  // Also see DieTemp Register (034h) for the internal die temperature p. 24 in DS  
+  // If Config.TSel = 0, DieTemp and Temp registers have the value of the die temperature.
   return readRegister16Bits(this->wire, FUEL_GAUGE_ADDRESS, TEMP_REG) * TEMPERATURE_MULTIPLIER_C;
 }
 
@@ -226,7 +226,6 @@ uint8_t Battery::batteryTemperature(){
   }
 
   setTemperatureMeasurementMode(true);
-  // TODO possibly await a couple of readings before getting a meaningful temperature
   return readRegister16Bits(this->wire, FUEL_GAUGE_ADDRESS, TEMP_REG) * TEMPERATURE_MULTIPLIER_C;
 }
 
